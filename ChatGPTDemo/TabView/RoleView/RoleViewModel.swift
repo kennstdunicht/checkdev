d3c2590d9e9e929f90ee4b74b2244d0b5a866e46
Pierre Burghardt <pierre.burghardt@check24.de>
2024-05-03 17:32:29 +0200

    girl

--- ChatGPTDemo/TabView/RoleView/RoleViewModel.swift
+++ ChatGPTDemo/TabView/RoleView/RoleViewModel.swift
@@ -10,6 +10,6 @@ import SwiftUI
 final class RoleViewModel: ObservableObject {
     let agents: [AgentRole] = [
         AgentRole(name: "Dev", model: .chat(.llama), address: "localhost", sprites: DuckyImages.idleBounce(), systemPrompt: "You are a Developer", temperature: 0.6),
-        AgentRole(name: "Dokumenter", model: .chat(.llama), address: "localhost", sprites: DuckyImages.walk(), systemPrompt: "You will explain the code you get", temperature: 0.6)
+        AgentRole(name: "Dokumenter", model: .chat(.llama), address: "localhost", sprites: GirlImages.idle(), systemPrompt: "You will explain the code you get", temperature: 0.6)
     ]
 }
