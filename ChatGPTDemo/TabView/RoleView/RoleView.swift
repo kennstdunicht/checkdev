3608e21751f5bf9dcae979fcd817c15a25745480
Pierre Burghardt <pierre.burghardt@check24.de>
2024-05-03 16:15:52 +0200

    wip role screen

--- ChatGPTDemo/TabView/RoleView/RoleView.swift
+++ ChatGPTDemo/TabView/RoleView/RoleView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct RoleView: View {
+    let agents: [AgentRole] = [
+        AgentRole(name: "Dev", model: .chat(.llama), address: "localhost", sprites: DuckyImages.idleBounce(), systemPrompt: "You are a Developer", temperature: 0.6),
+        AgentRole(name: "Dokumenter", model: .chat(.llama), address: "localhost", sprites: DuckyImages.walk(), systemPrompt: "You will explain the code you get", temperature: 0.6)
+    ]
+    
     var body: some View {
         VStack {
-            Text("Role View")
-            Spacer()
+            ForEach(0 ..< agents.count) { index in
+                let agent = agents[index]
+                HStack {
+                    AnimatedSprite(sprites: agent.sprites)
+                        .frame(width: 64, height: 64)
+                    
+                    Text(agent.name)
+                        .font(.system(size: 24, weight: .bold, design: .serif))
+                }
+            }
+            .padding(.horizontal, 32)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
